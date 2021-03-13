293b864324b444806688f534ad4713001a80167e
Esteban <esteban1.redon2@gmail.com>
2021-03-13 14:13:27 -0300

    Guardando cambios del backend

--- admin/index.php
+++ admin/index.php
@@ -1,12 +1,65 @@
 <?php
+
+    // Importar la base de datos
+    require '../includes/config/database.php';
+    $db = conectarDB();
+    // Escribir el query
+    $query = "SELECT * FROM propiedades";
+
+    // Consultar la BD
+    $resultadoConsulta = mysqli_query($db, $query);
+
+
+    // Muestra mensaje condicional
+    $resultado = $_GET['resultado'] ?? null;
+
+    // Incluye un template
     require '../includes/funciones.php';
     incluirTemplate('header');
 ?>
 
     <main class="contenedor seccion">
         <h1>Administrador de Bienes Raices</h1>
+        <?php if(intval($resultado) === 1): ?>
+            <p class="alerta exito">Anuncio creado correctamente</p>
+        <?php endif; ?>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
+
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody> <!-- Mostrar los resultados -->
+            <?php while($propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>
+                <tr>
+                    <td><?php echo $propiedad['id']; ?></td>
+                    <td><?php echo $propiedad['titulo']; ?></td>
+                    <td><img src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt="Imagen de propiedad" class="imagen-tabla"></td>
+                    <td><?php echo $propiedad['precio']; ?></td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-naranja-block">Actualizar</a>
+                    </td>
+                </tr>
+
+                <?php endwhile; ?>
+            </tbody>
+        </table>
     </main>
+
+<?php
+    // Cerrar la conexión
+    mysqli_close($db);
+
+?>
 
 <?php incluirTemplate('footer'); ?>
--- includes/config/database.php
+++ includes/config/database.php
@@ -2,6 +2,7 @@
 
 function conectarDB() : mysqli{
     $db = mysqli_connect('localhost', 'root', 'root', 'bienes_raices');
+    // $db -> set_charset('utf8');
 
     if (!$db) {
         echo "Error, no conectó";
